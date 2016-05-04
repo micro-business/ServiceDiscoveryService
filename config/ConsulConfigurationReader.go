ca5e176ed51d358fa302626a0f96999cea21e778
mortezaalizadeh <morteza.alizadeh@gmail.com>
2016-05-04 22:43:09 +1200

    Refactored ConsulConfigurationReader to use the new ConsulHelper in Micro-Business-Core

--- config/ConsulConfigurationReader.go
+++ config/ConsulConfigurationReader.go
@@ -1,12 +1,6 @@
 package config
 
-import (
-	"errors"
-	"fmt"
-	"strconv"
-
-	"github.com/hashicorp/consul/api"
-)
+import "github.com/microbusinesses/Micro-Businesses-Core/common/config"
 
 type ConsulConfigurationReader struct {
 	ConsulAddress           string
@@ -21,65 +15,8 @@ func (consul ConsulConfigurationReader) GetListeningPort() (int, error) {
 		return consul.ListeningPortToOverride, nil
 
 	} else {
-		return getInt(consul, serviceListeningPortKey)
-	}
-}
+		consulHelper := config.ConsulHelper{ConsulAddress: consul.ConsulAddress, ConsulScheme: consul.ConsulScheme}
 
-func getKV(consul ConsulConfigurationReader) (*api.KV, error) {
-	config := api.DefaultConfig()
-
-	if len(consul.ConsulAddress) != 0 && len(consul.ConsulScheme) != 0 {
-		config.Address = consul.ConsulAddress
-		config.Scheme = consul.ConsulScheme
-	}
-
-	if client, err := api.NewClient(config); err != nil {
-		return nil, err
-	} else {
-		return client.KV(), nil
-	}
-}
-
-func getKeyPair(consul ConsulConfigurationReader, configKeyPath string) (*api.KVPair, error) {
-	kv, err := getKV(consul)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if keyPair, _, err := kv.Get(configKeyPath, nil); err != nil {
-		return nil, err
-	} else {
-		return keyPair, nil
-	}
-}
-
-func getInt(consul ConsulConfigurationReader, keyPath string) (int, error) {
-	keyPair, err := getKeyPair(consul, keyPath)
-
-	if err != nil {
-		return 0, err
-	}
-
-	if keyPair == nil {
-		return 0, errors.New(fmt.Sprintf("Consul key %s does not exist.", keyPath))
-
-	}
-
-	valueInString := string(keyPair.Value)
-
-	if len(valueInString) == 0 {
-		return 0, errors.New(fmt.Sprintf("Consul key %s is empty.", keyPath))
-
-	}
-
-	if value, err := strconv.Atoi(valueInString); err != nil {
-		return 0, err
-	} else {
-		if value == 0 {
-			return 0, errors.New(fmt.Sprintf("Consul key %s is zero.", keyPath))
-		}
-
-		return value, nil
+		return consulHelper.GetInt(serviceListeningPortKey)
 	}
 }

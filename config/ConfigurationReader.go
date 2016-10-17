3dc72217c9a21cd7af0e7a5ce0bb04a874f08e80
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-10-17 20:36:08 +1300

    Refactored

--- config/ConfigurationReader.go
+++ config/ConfigurationReader.go
@@ -1,7 +1,13 @@
 package config
 
+// ConfigurationReader defines the interface that provides access to all configurations parameters required by the service.
 type ConfigurationReader interface {
+	// GetListeningPort returns the port the application should start listening on.
+	// Returns either the listening port or error if something goes wrong.
 	GetListeningPort() (int, error)
 
+	// GetOverrideHostname returns the overridden hostname to be used by the service discovery. If provided, the IP addresses or hostname
+	// provided by the service must be overridden by this parameter.
+	// This parameter is meant to be used in development environment.
 	GetOverrideHostname() (string, error)
 }
--- config/ConsulConfigurationReader.go
+++ config/ConsulConfigurationReader.go
@@ -2,6 +2,7 @@ package config
 
 import "github.com/microbusinesses/Micro-Businesses-Core/common/config"
 
+// ConsulConfigurationReader implements ConfigurationReader using Consul to provide access to all configurations parameters required by the service.
 type ConsulConfigurationReader struct {
 	ConsulAddress              string
 	ConsulScheme               string
@@ -12,24 +13,28 @@ type ConsulConfigurationReader struct {
 const serviceListeningPortKey = "services/service-discovery-service/endpoint/listening-port"
 const overrideHostnameKey = "services/service-discovery-service/endpoint/override-hostname"
 
+// GetListeningPort returns the port the application should start listening on.
+// Returns either the listening port or error if something goes wrong.
 func (consul ConsulConfigurationReader) GetListeningPort() (int, error) {
 	if consul.ListeningPortToOverride != 0 {
 		return consul.ListeningPortToOverride, nil
 
-	} else {
-		consulHelper := config.ConsulHelper{ConsulAddress: consul.ConsulAddress, ConsulScheme: consul.ConsulScheme}
-
-		return consulHelper.GetInt(serviceListeningPortKey)
 	}
+	consulHelper := config.ConsulHelper{ConsulAddress: consul.ConsulAddress, ConsulScheme: consul.ConsulScheme}
+
+	return consulHelper.GetInt(serviceListeningPortKey)
 }
 
+// GetOverrideHostname returns the overridden hostname to be used by the service discovery. If provided, the IP addresses or hostname
+// provided by the service must be overridden by this parameter.
+// This parameter is meant to be used in development environment.
 func (consul ConsulConfigurationReader) GetOverrideHostname() (string, error) {
 	if len(consul.OverrideHostnameToOverride) != 0 {
 		return consul.OverrideHostnameToOverride, nil
 
-	} else {
-		consulHelper := config.ConsulHelper{ConsulAddress: consul.ConsulAddress, ConsulScheme: consul.ConsulScheme}
-
-		return consulHelper.GetString(overrideHostnameKey)
 	}
+
+	consulHelper := config.ConsulHelper{ConsulAddress: consul.ConsulAddress, ConsulScheme: consul.ConsulScheme}
+
+	return consulHelper.GetString(overrideHostnameKey)
 }
